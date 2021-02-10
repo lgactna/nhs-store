c5fb69443e5ec850c38b761f66366ff7b018a5cd
lgactna <48928674+lgactna@users.noreply.github.com>
2021-02-09 17:14:29 -0800

    fix bad settings import, add urls

--- src/nhssite/nhssite/urls.py
+++ src/nhssite/nhssite/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('store/', include('store.urls')),
+    path('', RedirectView.as_view(url='store/', permanent=True)), #redir root to store (for now)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
