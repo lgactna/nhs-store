7ba9cc1fd4feebf412027cde2cba2f741f571549
lgactna <48928674+lgactna@users.noreply.github.com>
2021-02-18 16:44:58 -0800

    basic get/post on cart page

--- src/nhssite/store/views.py
+++ src/nhssite/store/views.py
@@ -29,6 +29,8 @@ def cart(request):
         cart = request.session.get('cart', {})
         request.session['cart'] = cart
     
+    new_item_id = str(time.time())
+
     if request.method == "POST":
         form = CartForm(request.POST)
         # Check if the form is valid:
@@ -38,14 +40,40 @@ def cart(request):
                 "quantity": form.cleaned_data['quantity'],
                 "material": form.cleaned_data['material'],
             }
-            request.session['cart'][str(time.time())] = cart_item
+            request.session['cart'][new_item_id] = cart_item
             request.session.modified = True
 
-            #show cart page here...
-            return render(request, 'index.html')
+    cart_contents = []
+    total_cost = 0
+    new_item = None
 
-    else:
-        pass
+    for cart_id, cart_item in request.session['cart'].items():
+        product_obj = Product.objects.get(id=cart_item['product_id'])
+        material_obj = Material.objects.get(id=cart_item['material'])
+        item_cost = int(cart_item['quantity'])*product_obj.price
+        cart_dict = {
+            "product": product_obj,
+            "quantity": cart_item['quantity'],
+            "material": material_obj,
+            "item_cost": item_cost
+        }
+        total_cost += item_cost
+
+        if cart_id == new_item_id:
+            new_item = cart_dict
+        else:
+            cart_contents.append(cart_dict)
+
+    context = {
+        "cart_contents": cart_contents,
+        "total_cost": total_cost,
+        "new_item": new_item
+    }
+    #show cart page here...
+    return render(request, 'cart.html', context=context)
+
+def cart_delete(request):
+    pass
 
 def checkout(request):
     pass
