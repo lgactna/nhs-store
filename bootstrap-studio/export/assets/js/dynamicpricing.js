15eb72076db964523f4d15791218a94e52baf017
lgactna <48928674+lgactna@users.noreply.github.com>
2021-02-19 11:12:31 -0800

    add price changer script on item page

---
+++ bootstrap-studio/export/assets/js/dynamicpricing.js
@@ -0,0 +1,6 @@
+var unit_price = 4;
+
+$("input[name='quantity']").on('input', function() {
+    var units = $("input[name='quantity']").val()
+    $("#total-price").text("$"+(unit_price*units).toFixed(2))
+});
